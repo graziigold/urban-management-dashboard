40d1d39f388f81cce58934a1da113d4e3220992f
Graziella Doche <grazigold804@gmail.com>
2026-05-27 10:15:57 -0300

    Update info.ts

--- src/utils/supabase/info.ts
+++ src/utils/supabase/info.ts
@@ -1,6 +1,6 @@
 // Supabase project configuration
-export const SUPABASE_URL = "https://zgltqzfjipjvojcmtcff.supabase.co";
-export const projectId = 'zgltqzfjipjvojcmtcff';
+export const SUPABASE_URL = "https://kqrmsxhmbjzwjnxhfnap.supabase.co";
+export const projectId = 'kqrmsxhmbjzwjnxhfnap';
 
 // Chave pública de autenticação
-export const publicAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnbHRxemZqaXBqdm9qY210Y2ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwODUyMDEsImV4cCI6MjA5NDY2MTIwMX0.ycppnnqQf98mlG6KFGgHz30TZCelxnP9smJQEi6muDk';
+export const publicAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_DQm7g2O-m4BohGzHD3npfQ_NJd6SBxj';
